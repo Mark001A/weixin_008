0346090e0ba3eb379cff6659da479da2bc524aab
752905288@qq.com <huang121105>
2017-08-07 07:22:28 +0800

    home  sj

--- app/src/main/java/hyj/weixin_008/Set008DataService.java
+++ app/src/main/java/hyj/weixin_008/Set008DataService.java
@@ -1,5 +1,6 @@
 package hyj.weixin_008;
 
+import android.accessibilityservice.AccessibilityService;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 
@@ -7,9 +8,11 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import hyj.weixin_008.service.ADBClickService;
 import hyj.weixin_008.util.FileUtil;
 import hyj.weixin_008.util.LogUtil;
 
@@ -19,12 +22,17 @@ import hyj.weixin_008.util.LogUtil;
 
 public class Set008DataService implements Runnable{
     static List<String[]> datas;
-    MyService context;
+    static Map<String,String> accounts;
+    String currentAccount;
+    AccessibilityService context;
     Map<String,String> record;
-    public Set008DataService(MyService context, Map<String,String> record){
+    ADBClickService adbService;
+    public Set008DataService(AccessibilityService context, Map<String,String> record){
         this.context = context;
         this.record = record;
+        adbService = new ADBClickService(context,record);
         datas = get008Datas();
+        accounts = getWxAccounts();
     }
     @Override
     public void run() {
@@ -37,31 +45,84 @@ public class Set008DataService implements Runnable{
                 AutoUtil.sleep(500);
                 continue;
             }
-
-            AccessibilityNodeInfo node1 = AutoUtil.findNodeInfosByText(root,"工具箱");
-            if(node1!=null){
-                AutoUtil.clickXY(373,422);
-                AutoUtil.recordAndLog(record,"点击图片");
-                AutoUtil.sleep(1000);
-                continue;
-            }
-            if(AutoUtil.checkAction(record,"点击图片")){
-                AccessibilityNodeInfo list = AutoUtil.findNodeInfosById(root,"com.soft.apk008v:id/set_value_con");
-                System.out.println("list--->"+list);
-                if(list!=null){
-                    System.out.println("size--->"+list.getChildCount());
-                }
-                if(list!=null&&list.getChildCount()==91){
-                    for(int i=1;i<91;i++){
-                        if(list.getChild(i).isEditable()){
-                            System.out.println("-rr->"+i+" "+datas.get(0)[i]);
-                            AutoUtil.performSetText(list.getChild(i),datas.get(1)[i+1],record,"写入"+i+" "+datas.get(0)[i+1]);
-                        }
-                    }
-                }
-            }
+            do008(root);
+            doWxLogin(root);
         }
     }
+
+    private void doWxLogin(AccessibilityNodeInfo root){
+        if(AutoUtil.checkAction(record,"写入数据")){
+            AutoUtil.showToastByRunnable(GlobalApplication.getContext().getApplicationContext(),"启动微信");
+            AutoUtil.startAppByPackName("com.tencent.mm","com.tencent.mm.ui.LauncherUI");
+            AutoUtil.sleep(5000);
+        }
+        adbService.clickXYByWindow("登录&注册",164,1192,"点击登录1",500);
+        if(!AutoUtil.checkAction(record,"输入手机号")){
+            adbService.setTextByWindow("用微信号/QQ号/邮箱登录",387,490,currentAccount,"输入手机号",0);
+        }
+        if(AutoUtil.findNodeInfosByText(root,currentAccount)!=null)
+            adbService.clickXYByWindow("用微信号/QQ号/邮箱登录",361,751,"下一步",1000);
+        System.out.println("密码---》"+accounts.get(currentAccount));
+        if(AutoUtil.checkAction(record,"下一步"))
+            adbService.setTextByWindow("用短信验证码登录",387,490,accounts.get(currentAccount),"输入密码",2000);
+        adbService.clickXYByWindow("用短信验证码登录",361,751,"登录2",5000);
+        adbService.clickXYByWindow("是&否",416,793,"不推荐通讯录",3000);
+        if(AutoUtil.checkAction(record,"不推荐通讯录")){
+            AutoUtil.clickXY(272,1228);
+        }
+        List<AccessibilityNodeInfo> node1 = root.findAccessibilityNodeInfosByText("微信团队");
+        List<AccessibilityNodeInfo> node2 = root.findAccessibilityNodeInfosByText("腾讯新闻");
+        if(node1!=null&&!node1.isEmpty()){
+            AutoUtil.showToastByRunnable(context,"登录成功");
+            AutoUtil.recordAndLog(record,"登录成功");
+            AutoUtil.sleep(3000);
+        }
+    }
+
+    private void do008(AccessibilityNodeInfo root){
+
+        adbService.clickXYByWindow("工具箱",373,422,"点击图片",1000);
+        if(AutoUtil.checkAction(record,"点击图片")||AutoUtil.checkAction(record,"清除数据")) {
+            set008Data(root);
+        }
+        if(AutoUtil.checkAction(record,"登录成功")){
+            AutoUtil.showToastByRunnable(GlobalApplication.getContext().getApplicationContext(),"启动008");
+            AutoUtil.startAppByPackName("com.soft.apk008v","com.soft.apk008.LoadActivity");
+            AutoUtil.recordAndLog(record,"wx-启动008");
+            AutoUtil.sleep(2000);
+        }
+        if(AutoUtil.checkAction(record,"wx-启动008")){
+            AutoUtil.clickXY(50,1226);
+            AutoUtil.sleep(1500);
+            AutoUtil.clickXY(376,622);
+            AutoUtil.recordAndLog(record,"清除数据");
+            AutoUtil.sleep(4000);
+        }
+    }
+    int currentIndex=0;
+    private void set008Data(AccessibilityNodeInfo root){
+        AccessibilityNodeInfo list = AutoUtil.findNodeInfosById(root,"com.soft.apk008v:id/set_value_con");
+        if(list!=null&&list.getChildCount()==91){
+            for(int i=1;i<91;i++){
+                if(list.getChild(i).isEditable()){
+                    String data  = datas.get(currentIndex)[i+1];
+                    System.out.println("-rr->"+i+" "+data);
+                    AutoUtil.performSetText(list.getChild(i),data,record,"写入"+i+" "+data);
+                }
+            }
+            AutoUtil.showToastByRunnable(GlobalApplication.getContext().getApplicationContext(),"总共:"+datas.size()+"  当前序号:"+currentIndex);
+            AutoUtil.sleep(500);
+            AutoUtil.clickXY(90,359);
+            currentAccount = datas.get(currentIndex)[datas.get(currentIndex).length-1];
+            if(currentIndex<datas.size()-1){
+                currentIndex = currentIndex+1;
+            }else {
+
+            }
+            AutoUtil.recordAndLog(record,"写入数据");
+        }
+    }
+
     private List<String[]> get008Datas(){
         List<String> list =  FileUtil.read008Data("/sdcard/A_hyj_008data/008data.txt");
         List<String[]> newList = new ArrayList<String[]>();
@@ -69,6 +130,15 @@ public class Set008DataService implements Runnable{
             newList.add(JSONObject.parseObject(s,String[].class));
         }
        return newList;
+    }
+    private Map<String,String>  getWxAccounts(){
+        Map<String,String> accounts = new HashMap<String,String>();
+        List<String[]> list =   FileUtil.readConfFile("/sdcard/wxAccounts.txt");
+        for(String[] str:list){
+            accounts.put(str[0],str[1]);
+        }
+        System.out.println("currentAccount-->"+accounts);
+        return accounts;
     }
     private int getStartIndex(){
         List<String[]> datas = get008Datas();
---
+++ app/src/main/java/hyj/weixin_008/service/ADBClickService.java
@@ -0,0 +1,63 @@
+package hyj.weixin_008.service;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityNodeInfo;
+
+import java.util.Map;
+
+import hyj.weixin_008.AutoUtil;
+import hyj.weixin_008.util.LogUtil;
+
+/**
+ * Created by asus on 2017/8/5.
+ */
+
+public class ADBClickService {
+    static final String TAG = "ADBClickService";
+    AccessibilityService context;
+    Map<String,String> record;
+    public ADBClickService(AccessibilityService context, Map<String,String> record){
+        this.record = record;
+        this.context = context;
+    }
+    public void clickXYByWindow(String windowNodeTexts,int clickX,int clickY,String action,long sleepTime){
+        if(!checkWindow(windowNodeTexts,action)) return;
+        AutoUtil.clickXY(clickX,clickY);
+        AutoUtil.recordAndLog(record,action);
+        AutoUtil.sleep(sleepTime);
+    }
+    public void setTextByWindow(String windowNodeTexts,int clickX,int clickY,String inputText,String action,long sleepTime){
+        System.out.println("-dddd-->"+checkWindow(windowNodeTexts,action));
+        if(!checkWindow(windowNodeTexts,action)) return;
+        AutoUtil.clickXY(clickX,clickY);
+        AutoUtil.sleep(1500);
+        AutoUtil.inputText(inputText);
+        AutoUtil.recordAndLog(record,action);
+        AutoUtil.sleep(sleepTime);
+    }
+    public boolean checkWindow(String windowNodeTexts,String action){
+        AccessibilityNodeInfo root = context.getRootInActiveWindow();
+        if(root==null){
+            LogUtil.d(TAG,"action "+action+" root is null!");
+            return false;
+        }
+        if(windowNodeTexts.contains("&")){
+            String[] str = windowNodeTexts.split("&");
+            for(String text:str){
+                AccessibilityNodeInfo node = AutoUtil.findNodeInfosByText(root,text);
+                if(node==null){
+                    LogUtil.d(TAG,"node's text  "+text+" is not find!");
+                    return false;
+                }
+            }
+
+        }else{
+            AccessibilityNodeInfo node = AutoUtil.findNodeInfosByText(root,windowNodeTexts);
+            if(node==null){
+                LogUtil.d(TAG,"node's text  "+windowNodeTexts+" is not find!");
+                return false;
+            }
+        }
+        return true;
+    }
+}
